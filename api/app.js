834a6dd46f23cab28b52291b2e5202584560b716
JanRockIT <programmieren001@gmail.com>
2025-03-30 19:54:08 +0200

    d

--- api/app.js
+++ api/app.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { createClient } from 'supabase/supabase-js';
+import { createClient } from '@supabase/supabase-js';
 
 const app = express();
 app.use(express.json());
